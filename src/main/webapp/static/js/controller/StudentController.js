0a8539748f9eeab0472fa470b085f526f0d785da
Dariy Pozinenko <dpozinen@gmail.com>
2018-11-28 22:09:28 +0200

    added js link

---
+++ src/main/webapp/static/js/controller/StudentController.js
@@ -0,0 +1,58 @@
+'use strict'
+
+angular.module('App').controller('StudentController', ['StudentService', '$scope', function(StudentService, $scope) {
+	var self = this;
+	self.students = [];
+	self.student = {id:null, firstName:'', lastName:'', faculty:'', speciality:'', cardID:''};
+
+	self.reset = reset;
+	self.remove = remove;
+	self.submit = submit;
+
+	getAllStudents();
+
+	function getAllStudents() {
+		StudentService.getAllStudents().then(
+			function(students) {
+				self.students = students;
+			},
+			function(errResponse) {
+				console.error('Couldn\'t get all Students');
+			}
+		);
+	}
+
+	function addStudent(student) {
+		StudentService.addStudent(student).then(
+			getAllStudents,
+			function(errResponse) {
+				console.error('Couldn\'t add student', student);
+			}
+		);
+	}
+
+	function deleteStudent(id) {
+		StudentService.deleteStudent(id).then(
+			getAllStudents,
+			function() {
+				console.error('Couldn\'t delete student', student);
+			}
+		);
+	}
+
+	function reset() {
+		self.student = {id:null, firstName:'', lastName:'', faculty:'', speciality:'', cardID:''};
+		$scope.studentForm.$setPristine();
+	}
+
+	function submit() {
+		console.log('Saving New student', self.student);
+		addStudent(self.student);
+		reset();
+	}
+
+	function remove(id) {
+		deleteStudent(id);
+	}
+
+}]);
